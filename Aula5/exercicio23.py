9db89b5162be7fe228ca1988caa24b97585a02e9
ArthurCaio <arthurcaiosantoslins@gmail.com>
2020-08-16 17:53:13 -0300

    atualização

---
+++ Aula5/exercicio23.py
@@ -0,0 +1,7 @@
+n = str(input('Digite um Numero: '))
+n = n.strip()
+u = n[3] + 0
+d = n[2] + 0
+c = n[1] + 0
+um = n[0] + 0
+print('unidade : {} \nDezena : {} \nCentena : {} \nUnidade de Milhar {}'.format(u,d,c,um))
