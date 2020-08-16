4cefb9398b07cded826a2c239c35118835e5d175
ArthurCaio <arthurcaiosantoslins@gmail.com>
2020-08-16 15:09:24 -0300

    repositorio-atividades

---
+++ Aula5/exercicio22.py
@@ -0,0 +1,10 @@
+n = str(input('Digite seu Nome Completo: '))
+nma = n.upper()
+nme = n.lower()
+nldpn = n.split()
+nldpn1 = len(nldpn[0][0:])
+ndlse = n.split() 
+print('Seu Nome Totalmente Maiusculo é: {} \nSeu Nome Totalmente Menosculo é: {} \nA Primeira Palavra do Seu Nome tem {} Letras'.format(nma,nme,nldpn1))
+print(len(ndlse[0:7
+
+]).)
