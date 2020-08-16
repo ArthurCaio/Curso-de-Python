7587aa2e4fc6e104d7b34db69e1f7f5193289fda
ArthurCaio <arthurcaiosantoslins@gmail.com>
2020-08-16 15:14:56 -0300

    Update exercicio22.py

--- Aula5/exercicio22.py
+++ Aula5/exercicio22.py
@@ -5,6 +5,4 @@ nldpn = n.split()
 nldpn1 = len(nldpn[0][0:])
 ndlse = n.split() 
 print('Seu Nome Totalmente Maiusculo é: {} \nSeu Nome Totalmente Menosculo é: {} \nA Primeira Palavra do Seu Nome tem {} Letras'.format(nma,nme,nldpn1))
-print(len(ndlse[0:7
-
-]).)
+print(len(ndlse[0:]).)
